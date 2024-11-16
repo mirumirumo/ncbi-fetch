ab778355155b8cb2c913d836f5bad9e4ff8377c6
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-11-16 21:43:01 +0900

    change the message which is send when connect command arensuccessfully run

--- cmd/connect.go
+++ cmd/connect.go
@@ -8,28 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const SERVER string = "ftp.ncbi.nlm.nih.gov:21"
-
 // catCmd represents the cat command
 var connectCmd = &cobra.Command{
 	Use:   "connect",
 	Short: "connect to the ftp server",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		c, cancel, err := connect.Connect()
+		_, cancel, err := connect.Connect()
 		defer cancel()
 		if err != nil {
 			log.Fatalf("Failed to connect: %v", err)
 		}
 
-		entries, err := c.List("")
 		if err != nil {
 			log.Fatalf("Failed to list directory: %v", err)
 		}
-
-		for _, entry := range entries {
-			fmt.Println(entry.Name)
-		}
+		fmt.Printf("You can successfully connect to the NCBI ftp server: %s\n", connect.SERVER)
 
 	},
 }
