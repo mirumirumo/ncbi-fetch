b3972cb1e893d12d827907d9f0907788a136e131
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-12-25 16:49:03 +0900

    enable to specify multiple species after the flag "species"

--- cmd/get/taxonid.go
+++ cmd/get/taxonid.go
@@ -7,24 +7,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var organism string
+var organisms []string
 
 var taxonidCmd = &cobra.Command{
 	Use:   "taxonid",
 	Short: "get the taxon id from the NCBI database",
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		if organism == "" {
+		if len(organisms) == 0 {
 			fmt.Println("Please specify the flag \"organism\" to get the taxon id")
 		}
-		taxonid, err := search.Org2Taxon(organism)
+		taxonids, err := search.Org2Taxon(organisms)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
-		fmt.Printf("Taxon ID for %s: %s\n", organism, taxonid)
+
+		fmt.Printf("%s", string(taxonids))
 	}}
 
 func init() {
 	GetCmd.AddCommand(taxonidCmd)
-	taxonidCmd.Flags().StringVarP(&organism, "species", "s", "", "an organism name to get the taxon id")
+	taxonidCmd.Flags().StringSliceVarP(&organisms, "species", "s", nil, "an organism name to get the taxon id")
 }
--- search/org2taxon.go
+++ search/org2taxon.go
@@ -1,7 +1,9 @@
 package search
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -11,33 +13,46 @@ type SearchResult struct {
 	IDs []string `xml:"IdList>Id"`
 }
 
-func Org2Taxon(org string) (string, error) {
+type Taxonid struct {
+	Species string `json:"species"`
+	Taxonid string `json:"taxon_id"`
+}
+
+func Org2Taxon(orgs []string) ([]byte, error) {
 	baseURL := "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-	params := url.Values{}
-	params.Add("db", "taxonomy")
-	params.Add("term", org)
-	params.Add("retmode", "xml")
-	params.Add("retmax", "1")
+	var taxonIDs []Taxonid
 
-	resp, err := http.Get(baseURL + "?" + params.Encode())
+	for _, org := range orgs {
+		params := url.Values{}
+		params.Add("db", "taxonomy")
+		params.Add("term", org)
+		params.Add("retmode", "xml")
+		params.Add("retmax", "1")
+
+		resp, err := http.Get(baseURL + "?" + params.Encode())
+		if err != nil {
+			return nil, fmt.Errorf("failed to get: %w", err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read body: %w", err)
+		}
+		var result SearchResult
+		if err := xml.Unmarshal(body, &result); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal xml: %w", err)
+		}
+
+		if len(result.IDs) > 0 {
+			taxonIDs = append(taxonIDs, Taxonid{Species: org, Taxonid: result.IDs[0]})
+		} else {
+			return nil, nil
+		}
+	}
+	out, err := json.Marshal(taxonIDs)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("failed to marshal json: %w", err)
 	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var result SearchResult
-	if err := xml.Unmarshal(body, &result); err != nil {
-		return "", err
-	}
-
-	if len(result.IDs) > 0. {
-		return result.IDs[0], nil
-	} else {
-		return "", nil
-	}
-
+	return out, nil
 }
