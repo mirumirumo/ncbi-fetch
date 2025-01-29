117b4c693858de5a4c988e4b4886531bc94e706c
mirumirumo <yoshinouchi.m.aa@gmail.com>
2025-01-29 16:40:54 +0900

    configの修正

--- connect/ftp.go
+++ connect/ftp.go
@@ -5,17 +5,10 @@ import (
 	"time"
 
 	"github.com/jlaffaye/ftp"
-	"github.com/secsy/goftp"
 )
 
 func ConnectFtp() (*ftp.ServerConn, func(), error) {
 	// FTPクライアントの設定
-	config := goftp.Config{
-		User:     USER,
-		Password: PASS,
-	}
-
-	_, err := goftp.DialConfig(config, HOST)
 	c, err := ftp.Dial(fmt.Sprintf("%s:%d", HOST, PORT), ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
 		return nil, nil, err
